55ca67e44e017a2cac42dc9da6cdeea68ff416f3
hiedo <68775340+hiedo@users.noreply.github.com>
2020-07-27 19:05:39 +0800

    实现登出功能

--- src/main/java/com/zsc/blog/web/client/LogOut.java
+++ src/main/java/com/zsc/blog/web/client/LogOut.java
@@ -2,16 +2,14 @@ package com.zsc.blog.web.client;
 
 import com.zsc.blog.Utils.responData.CodeEnum;
 import com.zsc.blog.Utils.responData.ResponseData;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 public class LogOut {
-    @ResponseBody
     @PostMapping("user/logout")
     public ResponseData<Map<String,String>> logout(@RequestHeader("token") String token) {
         return ResponseData.out(CodeEnum.SUCCESS, null);
