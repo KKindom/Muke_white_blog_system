0eeed25ce093ee9c193d0209dbf2cf0a75ad4ecb
hiedo <68775340+hiedo@users.noreply.github.com>
2020-07-28 21:27:52 +0800

    增加权限错误响应类型

--- src/main/java/com/zsc/blog/Utils/responData/CodeEnum.java
+++ src/main/java/com/zsc/blog/Utils/responData/CodeEnum.java
@@ -1,8 +1,5 @@
 package com.zsc.blog.Utils.responData;
 
-import lombok.Data;
-import org.omg.CORBA.PUBLIC_MEMBER;
-
 /**
  * 统一返回状态码
  */
@@ -15,8 +12,8 @@ public enum CodeEnum {
     FAILURE_no_username(500,"对不起，没有查询到该用户名！请检查是否输入错误！"),
     /* 密码错误登录失败*/
     FAILURE_error_password(500,"对不起，密码错误，请检查密码是否有误！"),
-    /* 存在此用户失败码*/
-    FAILURE_error_username(500,"对不起，已存在此用户！");
+    /* 权限错误登陆失败*/
+    FAILURE_illegal_account(500,"对不起，没有管理员权限！");
 
     public int code;
     public String  msg;
